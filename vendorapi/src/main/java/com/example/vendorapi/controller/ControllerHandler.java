86bb9602fc9c897355314dff4844e9891f04ec5d
starkedith01 <vihaankumarme@gmail.com>
2024-09-22 17:50:10 +0530

    modified:   .idea/vcs.xml 	modified:   vendorapi/src/main/java/com/example/vendorapi/controller/ControllerHandler.java 	new file:   vendorapi/src/main/java/com/example/vendorapi/response/ResponseHandler.java

--- vendorapi/src/main/java/com/example/vendorapi/controller/ControllerHandler.java
+++ vendorapi/src/main/java/com/example/vendorapi/controller/ControllerHandler.java
@@ -1,11 +1,15 @@
 package com.example.vendorapi.controller;
 
 import com.example.vendorapi.model.VendorData;
+import com.example.vendorapi.response.ResponseHandler;
 import com.example.vendorapi.service.VendorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @RestController
@@ -18,19 +22,22 @@ public class ControllerHandler {
     public  List<VendorData> getAllVendor(){
         return vendorService.getAllVendor();
     }
+
     @PostMapping
     public VendorData createVendor(@RequestBody VendorData vendorData){
                return  vendorService.createVendor(vendorData);
 
     }
+
     @DeleteMapping ("{id}")
     public String deleteById(@PathVariable("id") int id){
         vendorService.deleteVendorById(id);
         return "Vendor is deleted from the database";
     }
     @GetMapping("/{id}")
-    public Optional<VendorData> getVendorById(@PathVariable("id") int id){
-       return vendorService.getVendorById(id);
+    public ResponseEntity<Object> getVendorById(@PathVariable("id") int id){
+       return  ResponseHandler.responsebuilder("Requested Vendor Details are given here",
+               HttpStatus.OK,vendorService.getVendorById(id));
     }
     @PutMapping("/{id}")
     public VendorData updateVendorById(@PathVariable("id") int id ,@RequestBody VendorData vendorData){
