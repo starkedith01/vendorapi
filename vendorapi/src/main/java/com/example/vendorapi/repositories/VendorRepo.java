162cde1f84b79824c40ffb1985d97e00683cfd77
starkedith01 <vihaankumarme@gmail.com>
2024-09-22 11:58:49 +0530

    modified:   vendorapi/src/main/java/com/example/vendorapi/repositories/VendorRepo.java

--- vendorapi/src/main/java/com/example/vendorapi/repositories/VendorRepo.java
+++ vendorapi/src/main/java/com/example/vendorapi/repositories/VendorRepo.java
@@ -1,6 +1,5 @@
 package com.example.vendorapi.repositories;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.repository.CrudRepository;
 import com.example.vendorapi.model.VendorData;
 
